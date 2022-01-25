bd0d9eb4c7bf8e7780e57630d2c7e2b3c252213c
clappingmin <clappingmin@naver.com>
2022-01-25 18:51:37 +0900

    타입스크립트 설치

---
+++ nomad-react-master/react-masterclass2/src/App.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+// theme 사용하기
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>안녕하세요</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
