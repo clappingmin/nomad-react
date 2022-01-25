b4336180b3849de8f896066a4416e87fa60ef308
clappingmin <clappingmin@naver.com>
2022-01-25 19:51:27 +0900

    타입스크립트 인터페이스

--- nomad-react-master/react-masterclass2/src/Circle.tsx
+++ nomad-react-master/react-masterclass2/src/Circle.tsx
@@ -1,10 +1,33 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+interface ContainerProps {
+  bgColor: string;
+}
 
-function Circle() {
-  return;
-  <Container></Container>;
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50%;
+`;
+
+interface CircleProp {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProp) {
+  return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
+
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old`;
+
+sayHello({ name: 'sumin', age: 12 });
+sayHello({ name: 'hi', age: 12 });
