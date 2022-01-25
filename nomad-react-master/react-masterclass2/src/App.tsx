b04c799752c01f31f780a624e66aca8ab526638a
clappingmin <clappingmin@naver.com>
2022-01-25 23:06:21 +0900

    #3 typescript

--- nomad-react-master/react-masterclass2/src/App.tsx
+++ nomad-react-master/react-masterclass2/src/App.tsx
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import Circle from './Circle';
 
 function App() {
+  const [value, setValue] = useState('');
+  // 타입스크립트는 이 onChange 함수가 InputElement에 의해서 실행될 것을 안다.
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    // target이랑 currentTarget이랑 같은거임
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value); // 타입스크립트가 현재 setValue는 string을 받아야 한다는 걸 검사하고 있고,
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // - 기본 동작 방지 (새로고침)
+    console.log('hello', value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" />
+      {/* props 예제 */}
+      <Circle text="Im here" borderColor="yellow" bgColor="teal" />
       <Circle bgColor="tomato" />
+
+      {/* form 예제 */}
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
--- nomad-react-master/react-masterclass2/src/Circle.tsx
+++ nomad-react-master/react-masterclass2/src/Circle.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,14 +11,23 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 50%;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProp {
   bgColor: string;
+  borderColor?: string; // optinal
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProp) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = 'default text' }: CircleProp) {
+  // const [value, setValue] = useState<number | string>(1);
+  // setValue('hello');
+  return (
+    <Container borderColor={borderColor ?? bgColor} bgColor={bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
@@ -26,6 +37,7 @@ interface PlayerShape {
   age: number;
 }
 
+// 다른 예제
 const sayHello = (playerObj: PlayerShape) =>
   `Hello ${playerObj.name} you are ${playerObj.age} years old`;
 
